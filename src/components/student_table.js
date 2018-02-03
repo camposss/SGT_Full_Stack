b32954c39ead097c8536ffe1883b896806243307
camposss <ccamposs23@gmail.com>
2018-02-03 09:58:33 -0800

    confirm delete modal completed and fully functional. Need to work on css for modal and start on updating student data

--- src/components/student_table.js
+++ src/components/student_table.js
@@ -1,26 +1,57 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchStudentData,deleteStudent} from "../actions/";
+import '../assets/css/modal.css';
 
 
 class StudentTable extends Component{
-    // constructor(props){
-    //     super(props);
-    //
-    //     this.state= {
-    //     }
-    // }
+    constructor(props){
+        super(props);
+
+        this.state= {
+            deleteModal: false,
+            studentIndex: null
+        };
+        this.confirmDeleteModal= this.confirmDeleteModal.bind(this);
+    }
     componentDidMount() {
         this.props.fetchStudentData().then(()=>{
             console.log('these are the props in student table ', this. props);
         });
     }
-    handleDelete(index){
-        const studentId= this.props.students[index].id;
+    handleDelete(){
+        // console.log("this is the state after clicking delete button", index);
+        const studentId= this.props.students[this.state.studentIndex].id;
         console.log('this is the index of what you clicked ', studentId);
         this.props.deleteStudent(studentId).then(()=>{
-            this.props.fetchStudentData();
+            this.props.fetchStudentData().then(()=>{
+                this.setState({
+                    deleteModal: false
+                })
+            });
         });
+    }
+    confirmDeleteModal(){
+        // return PetListModal(this.state, self );
+        const {studentIndex}= this.state;
+        const index= this.props.students[studentIndex];
+        return(
+            <span>
+          <div className='confirm-modal '>
+              <div className="content-modal">
+                  <div className="card">
+                      <div className="card-header">Are you sure you want to delete:</div>
+                      <div className="card-block">
+                      </div>
+                      <div className="card-footer">
+                            <button onClick={()=> this.handleDelete()} className='btn btn-outline-success'>Confirm</button>
+                            <button onClick={()=> this.setState({deleteModal: false, canDelete: false})} className='btn btn-outline-danger'>Cancel</button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+        </span>
+        )
     }
     render(){
         const studentList = this.props.students.map((item,index)=>{
@@ -30,7 +61,7 @@ class StudentTable extends Component{
                         <td>{item.name}</td>
                         <td>{item.course}</td>
                         <td>{item.grade}</td>
-                        <td><button onClick= {()=>{this.handleDelete(index)}} className='btn btn-danger'>Delete</button></td>
+                        <td><button onClick= {()=>{this.setState({deleteModal: true, studentIndex:index})}} className='btn btn-danger'>Delete</button></td>
                     </tr>
                 )
             }
@@ -48,6 +79,7 @@ class StudentTable extends Component{
                 <tbody>
                 {studentList}
                 </tbody>
+                {this.state.deleteModal? this.confirmDeleteModal(): ''}
             </table>
 
 
