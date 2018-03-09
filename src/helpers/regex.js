5906c87adff31ff8d6a33820fcb4c727475f6e98
camposss <ccamposs23@gmail.com>
2018-03-09 14:36:06 -0800

    Edited mysql database to allow international names and restrict numebrs. Updated regex code to allow for international languages. This may allow the addition of special characters.

--- src/helpers/regex.js
+++ src/helpers/regex.js
@@ -1,7 +1,10 @@
 export function validateName (name){
-    const nameRegex = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
+    // const nameRegex = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
+    const nameRegex = /^.+$/;
+    const anyNumberRegex= /\d/;
+    const checkForNumber = anyNumberRegex.test(name);
     const testName= nameRegex.test(name);
-    if(!testName) return name;
+    if(!testName || checkForNumber) return name;
 }
 export function validateNumber(number){
     const numberRegex= /^[0-9]$|^[1-9][0-9]$|^(100)$/;
